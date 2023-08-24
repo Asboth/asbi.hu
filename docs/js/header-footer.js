3e91e9da15387891b017e58ef175ec86261b6ec4
Istvan Asboth <a@asbi.hu>
2023-08-24 10:02:40 +0200

    update

--- docs/js/header-footer.js
+++ docs/js/header-footer.js
@@ -38,4 +38,4 @@ function footerContainer(){
 					write("<div class='footerLnk'><a href='#'>Home</a> | <a href='#'>About Us</a> | <a href='#'>Contact Us</a></div>");					
 				write("</div>");
 			}
-	}
+	}*/
